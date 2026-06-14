e3b988323f1f95430357ee6392878b8d2022fe45
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-06-15 03:21:23 +0600

    add there

--- array.py
+++ array.py
@@ -1,1 +1,22 @@
-print("Hello, World!")
+# print("Hello, World!")
+# # sajano ar jonno 
+# short
+array = [1, 2, 3, 4, 5]
+# print(array)
+# append = array.append(6)
+# print(array)
+# remove = array.remove(3)
+# print(array)
+# sort = array.sort()
+# print(array)
+# reverse = array.reverse()
+# print(array)
+# # array er length ber kora
+# length = len(array)
+# print(length)
+# # array er index ber kora
+# index = array.index(4)
+# print(index)
+# array er count ber kora
+count = array.count(2)
+print(count)
