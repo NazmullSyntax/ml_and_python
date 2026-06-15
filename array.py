f90b27bb30413d8fe30c4e4bfda5e57890167a4f
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-06-16 02:38:13 +0600

    add some index in array

--- array.py
+++ array.py
@@ -1,22 +1,55 @@
-# print("Hello, World!")
-# # sajano ar jonno 
-# short
-array = [1, 2, 3, 4, 5]
+# # # print("Hello, World!")
+# # # # sajano ar jonno 
+# # # short
+# # array = [1, 2, 3, 4, 5]
+# # # print(array)
+# # # append = array.append(6)
+# # # print(array)
+# # # remove = array.remove(3)
+# # # print(array)
+# # # sort = array.sort()
+# # # print(array)
+# # # reverse = array.reverse()
+# # # print(array)
+# # # # array er length ber kora
+# # # length = len(array)
+# # # print(length)
+# # # # array er index ber kora
+# # # index = array.index(4)
+# # # print(index)
+# # # array er count ber kora
+# # count = array.count(2)
+# # print(count)
+# #append
+# array = [1, 2, 3, 4, 5]
+# array.append(6)
 # print(array)
-# append = array.append(6)
+# #remove
+# array.remove(3)
 # print(array)
-# remove = array.remove(3)
+# #sort
+# array.sort()
 # print(array)
-# sort = array.sort()
+# #reverse
+# array.reverse()
 # print(array)
-# reverse = array.reverse()
-# print(array)
-# # array er length ber kora
+# #length
 # length = len(array)
 # print(length)
-# # array er index ber kora
+# #index
 # index = array.index(4)
 # print(index)
-# array er count ber kora
-count = array.count(2)
-print(count)
+# #count
+# count = array.count(2)
+# print(count)
+# insert = array.insert(2, 10)
+# print(array)
+# snack = ["apple", "banana", "orange"]
+# snack.append("grape")
+# print(snack)
+snack = ["apple", "banana", "orange"]
+snack[1] = "grape"
+print(snack)
+
+
+
