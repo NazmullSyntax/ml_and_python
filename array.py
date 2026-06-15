3a526d3370a16aa66ce90d860dd325b09720713f
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-06-16 02:44:26 +0600

    add data structure in arrayu itemn

--- array.py
+++ array.py
@@ -1,55 +1,66 @@
-# # # print("Hello, World!")
-# # # # sajano ar jonno 
-# # # short
+# # # # print("Hello, World!")
+# # # # # sajano ar jonno 
+# # # # short
+# # # array = [1, 2, 3, 4, 5]
+# # # # print(array)
+# # # # append = array.append(6)
+# # # # print(array)
+# # # # remove = array.remove(3)
+# # # # print(array)
+# # # # sort = array.sort()
+# # # # print(array)
+# # # # reverse = array.reverse()
+# # # # print(array)
+# # # # # array er length ber kora
+# # # # length = len(array)
+# # # # print(length)
+# # # # # array er index ber kora
+# # # # index = array.index(4)
+# # # # print(index)
+# # # # array er count ber kora
+# # # count = array.count(2)
+# # # print(count)
+# # #append
 # # array = [1, 2, 3, 4, 5]
-# # # print(array)
-# # # append = array.append(6)
-# # # print(array)
-# # # remove = array.remove(3)
-# # # print(array)
-# # # sort = array.sort()
-# # # print(array)
-# # # reverse = array.reverse()
-# # # print(array)
-# # # # array er length ber kora
-# # # length = len(array)
-# # # print(length)
-# # # # array er index ber kora
-# # # index = array.index(4)
-# # # print(index)
-# # # array er count ber kora
+# # array.append(6)
+# # print(array)
+# # #remove
+# # array.remove(3)
+# # print(array)
+# # #sort
+# # array.sort()
+# # print(array)
+# # #reverse
+# # array.reverse()
+# # print(array)
+# # #length
+# # length = len(array)
+# # print(length)
+# # #index
+# # index = array.index(4)
+# # print(index)
+# # #count
 # # count = array.count(2)
 # # print(count)
-# #append
-# array = [1, 2, 3, 4, 5]
-# array.append(6)
-# print(array)
-# #remove
-# array.remove(3)
-# print(array)
-# #sort
-# array.sort()
-# print(array)
-# #reverse
-# array.reverse()
-# print(array)
-# #length
-# length = len(array)
-# print(length)
-# #index
-# index = array.index(4)
-# print(index)
-# #count
-# count = array.count(2)
-# print(count)
-# insert = array.insert(2, 10)
-# print(array)
+# # insert = array.insert(2, 10)
+# # print(array)
+# # snack = ["apple", "banana", "orange"]
+# # snack.append("grape")
+# # print(snack)
 # snack = ["apple", "banana", "orange"]
-# snack.append("grape")
+# snack[1] = "grape"
 # print(snack)
 snack = ["apple", "banana", "orange"]
-snack[1] = "grape"
+snack.insert(1, "grape")
 print(snack)
+del snack[2]
+print(snack)
+popped_snack = snack.pop(1)
+print(popped_snack)
+print(snack)
+cleared_snack = snack.clear()
+print(cleared_snack)
+
 
 
 
