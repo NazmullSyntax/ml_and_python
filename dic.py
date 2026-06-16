71bad3d61bf4d47239ac9ce0f1cac1444dd0d9ab
NazmullSyntax <nazmullsiddik17@gmail.com>
2026-06-16 16:01:20 +0600

    ass

--- dic.py
+++ dic.py
@@ -1,18 +1,34 @@
-print("Hello, World!")
-my_dict = {"name": "Alice", "age": 30, "city": "New York"}
-print(my_dict)
-print(my_dict["name"])
-my_dict["age"] = 31
-print(my_dict)
-my_dict["country"] = "USA"
-print(my_dict)
-del my_dict["city"]
-print(my_dict)
-for key, value in my_dict.items():
-    print(f"{key}: {value}")
-if "name" in my_dict:
-    print("Name is in the dictionary.")
-else:
-    print("Name is not in the dictionary.")
-my_dict.clear()
-print(my_dict)
+# print("Hello, World!")
+# my_dict = {"name": "Alice", "age": 30, "city": "New York"}
+# print(my_dict)
+# print(my_dict["name"])
+# my_dict["age"] = 31
+# print(my_dict)
+# my_dict["country"] = "USA"
+# print(my_dict)
+# del my_dict["city"]
+# print(my_dict)
+# for key, value in my_dict.items():
+#     print(f"{key}: {value}")
+# if "name" in my_dict:
+#     print("Name is in the dictionary.")
+# else:
+#     print("Name is not in the dictionary.")
+# my_dict.clear()
+# print(my_dict)
+# my_dict = {"name": "Alice", "age": 30, "city": "New York"}
+# print(my_dict.get("name"))
+# print(my_dict.get("age"))
+# print(my_dict.get("country", "Country not found"))
+# #pop
+# my_dict.pop("age")
+# print(my_dict)
+# #remove
+# my_dict.popitem()
+# print(my_dict)
+set_dict = {"apple", "banana", "cherry"}
+print(set_dict)
+for item in set_dict:
+    print(item)
+
+
